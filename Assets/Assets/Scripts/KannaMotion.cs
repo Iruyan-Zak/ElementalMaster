6736fd20ea1b1631a4586f1b9c9923d7e4447c1e
Iruyan_Zak <yuri.kazuna@gmail.com>
2017-11-05 19:40:03 +0900

    不完全だけれど、かんなちゃんが走る。

---
+++ Assets/Assets/Scripts/KannaMotion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KannaMotion : StateMachineBehaviour
+{
+    Rigidbody2D rigidbody;
+    KannaOperation kannaOperation;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        rigidbody = animator.GetComponent<Rigidbody2D>();
+        kannaOperation = animator.GetComponent<KannaOperation>();
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        animator.SetFloat("Speed", kannaOperation.IsGrounded ? Mathf.Abs(rigidbody.velocity.x) : 0);
+    }
+}
