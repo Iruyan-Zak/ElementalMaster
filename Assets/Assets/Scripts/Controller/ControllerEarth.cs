6578b1540006af0c8ce60ec98a43abcb286ccaf2
Iruyan_Zak <yuri.kazuna@gmail.com>
2017-11-05 22:38:37 +0900

    ダイヤリフトを実装。

--- Assets/Assets/Scripts/Controller/ControllerEarth.cs
+++ Assets/Assets/Scripts/Controller/ControllerEarth.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class ControllerEarth : ControllerDefault
 {
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+
+        ResetVelocity();
+    }
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+
+        ResetVelocity();
+        rigidbody.AddForce(JumpForce * Vector2.up, ForceMode2D.Impulse);
+    }
+
     protected override void Jump() { }
+
+    void ResetVelocity()
+    {
+        var velocity = rigidbody.velocity;
+        velocity.y = 0;
+        rigidbody.velocity = velocity;
+    }
 }
---
+++ Assets/Assets/Scripts/Gimmicks/DiamondLift.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondLift : MonoBehaviour
+{
+    float OriginY;
+    public float SpringCoef = 2;
+    public float DumperCoef = 2;
+
+    new Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        OriginY = rigidbody.mass * 10 /* 重力加速度 */ / SpringCoef + transform.position.y;
+    }
+
+    private void FixedUpdate()
+    {
+        var force = (OriginY - transform.position.y) * SpringCoef;
+        force -= rigidbody.velocity.y * DumperCoef;
+        rigidbody.AddForce(force * Vector2.up);
+    }
+}
