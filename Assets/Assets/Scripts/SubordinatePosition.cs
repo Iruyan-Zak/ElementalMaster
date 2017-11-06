5fa1d98b4fc8fcb781c187cd9bf159224299348e
Iruyan_Zak <yuri.kazuna@gmail.com>
2017-11-06 19:11:39 +0900

    かんなちゃんの判定位置を修正。ギミックに上から挟まれて死なないように。

---
+++ Assets/Assets/Scripts/SubordinatePosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubordinatePosition : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Target;
+
+    Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Target.transform.position + offset;
+    }
+}
